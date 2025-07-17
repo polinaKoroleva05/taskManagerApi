981792f2fe20d5af28ffdd2b23620b3a501ec06a
Test User <polina.koroleva05@mail.ru>
2025-07-17 14:02:50 +0300

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -81,7 +81,7 @@ function patchTask(id: number, taskData: TaskInterface) {
 }
 
 function save() {
-    fs.writeFileSync('./dist/db.json', JSON.stringify(base), (err: any) => {
+    fs.writeFileSync('./db.json', JSON.stringify(base), (err: any) => {
         if (err) throw err;
     });
 }
