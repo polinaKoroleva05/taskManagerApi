cc47a7dca76399ee1e49b9d385d9f73cd3d3a39f
Test User <polina.koroleva05@mail.ru>
2025-07-17 14:20:53 +0300

    test filewriting

--- api/index.ts
+++ api/index.ts
@@ -3,15 +3,15 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 const fs = require('fs');
-const cors = require('cors')
+const cors = require('cors');
 import {Request, Response} from 'express';
 
 const urlencodedParser = bodyParser.urlencoded({extended: false});
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 let base = require('./db.json');
 const filePathInScriptDir = path.join(__dirname, 'base.json');
-console.log(filePathInScriptDir)
+console.log(filePathInScriptDir);
 let tmpId: number = 7;
 
 type Category = 'Bug' | 'Feature' | 'Documentation' | 'Refactor' | 'Test';
@@ -35,38 +35,41 @@ app.get('/tasks', (req: Request, res: Response) => {
 app.get('/tasks/:id', (req: Request, res: Response) => {
     const id = +req.params.id;
     let task = base.find((task: TaskInterface) => task.id === id);
-    if(task){
+    if (task) {
         res.json(task);
-    }else{
-        res.sendStatus(404)
+    } else {
+        res.sendStatus(404);
     }
 });
 app.delete('/tasks/:id', (req: Request, res: Response) => {
     const id = +req.params.id;
     const deleteInd = base.findIndex((task: TaskInterface) => task.id === id);
-    if(deleteInd !== -1){
+    if (deleteInd !== -1) {
         base.splice(deleteInd, 1);
         res.json(base);
         save();
-    }
-    else{
-        res.sendStatus(404)
+    } else {
+        res.sendStatus(404);
     }
 });
 app.patch('/tasks/:id', (req: Request, res: Response) => {
-    const id = +req.params.id;
-    patchTask(id, req.body);
-    res.sendStatus(201);
-    save();
+    try {
+        const id = +req.params.id;
+        patchTask(id, req.body);
+        res.sendStatus(201);
+        save();
+    } catch (err) {
+        res.send(err);
+    }
 });
 app.post('/tasks', urlencodedParser, (req: Request, res: Response) => {
-    try{
-        console.log(req)
+    try {
+        console.log(req);
         createTask(req.body);
         save();
         res.sendStatus(201);
-    }catch(err){
-        res.send(err)
+    } catch (err) {
+        res.send(err);
     }
 });
 
@@ -84,6 +87,24 @@ function patchTask(id: number, taskData: TaskInterface) {
 
 function save() {
     fs.writeFileSync(filePathInScriptDir, JSON.stringify(base), (err: any) => {
+        if (err) throw err;
+    });
+    fs.writeFileSync('db.json', JSON.stringify(base), (err: any) => {
+        if (err) throw err;
+    });
+    fs.writeFileSync('./db.json', JSON.stringify(base), (err: any) => {
+        if (err) throw err;
+    });
+    fs.writeFileSync('./dist/db.json', JSON.stringify(base), (err: any) => {
+        if (err) throw err;
+    });
+    fs.writeFileSync('dist/db.json', JSON.stringify(base), (err: any) => {
+        if (err) throw err;
+    });
+    fs.writeFileSync('./api/db.json', JSON.stringify(base), (err: any) => {
+        if (err) throw err;
+    });
+    fs.writeFileSync('/api/db.json', JSON.stringify(base), (err: any) => {
         if (err) throw err;
     });
 }
