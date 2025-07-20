0de03928e7b1756320fb97793e7d666c495260d7
Test User <polina.koroleva05@mail.ru>
2025-07-20 13:25:01 +0300

    add jsdoc and error handling

--- api/index.ts
+++ api/index.ts
@@ -6,18 +6,26 @@ const path = require('path');
 const fs = require('fs');
 import {Request, Response} from 'express';
 
-
 const urlencodedParser = bodyParser.urlencoded({extended: false});
 const filePathInScriptDir = path.join(__dirname, 'db.json');
 app.use(express.json());
 app.use(cors());
 
-let tmpId: number = 7;
+let tmpId: number = 6;
 
 type Category = 'Bug' | 'Feature' | 'Documentation' | 'Refactor' | 'Test';
 type Status = 'To Do' | 'In Progress' | 'Done';
 type Priority = 'Low' | 'Medium' | 'High';
 
+/**
+ * @param {number | null} id - The id of task. Id = null, when client send new data to server. Server always send data with correct id.
+ * @param {string} title - The title of task.
+ * @param {string} [description] - The description of task. May be missing
+ * @param {Category} category - The category of task. May be 'Bug' | 'Feature' | 'Documentation' | 'Refactor' | 'Test'
+ * @param {Status} status - The status of task. May be 'To Do' | 'In Progress' | 'Done'
+ * @param {Priority} priority - The status of task. May be 'Low' | 'Medium' | 'High'
+ * @param {number | null} date - The date of creation task. Date = null, when client send new data to server. Server always send data with correct date.
+ */
 export interface TaskInterface {
     id: number | null;
     title: string;
@@ -28,12 +36,17 @@ export interface TaskInterface {
     date: number | null;
 }
 
+/**
+ * Get all tasks
+ */
 app.get('/tasks', (req: Request, res: Response) => {
     let base = require('./db.json');
     res.setHeader('Cache-Control', 'no-store, max-age=0');
     res.json(base);
 });
-
+/**
+ * Get task by id, if cant fiind - send status 404
+ */
 app.get('/tasks/:id', (req: Request, res: Response) => {
     let base = require('./db.json');
     const id = +req.params.id;
@@ -45,57 +58,101 @@ app.get('/tasks/:id', (req: Request, res: Response) => {
         res.sendStatus(404);
     }
 });
+
+/**
+ * Delete task by id, if cant find - send status 404. 
+ * If cause error, send status 500
+ */
 app.delete('/tasks/:id', (req: Request, res: Response) => {
     let base = require('./db.json');
     const id = +req.params.id;
     const deleteInd = base.findIndex((task: TaskInterface) => task.id === id);
     if (deleteInd !== -1) {
         base.splice(deleteInd, 1);
-        save(base)
-        res.setHeader('Cache-Control', 'no-store, max-age=0');
-        res.json(base);
+        try {
+            save(base);
+            res.setHeader('Cache-Control', 'no-store, max-age=0');
+            res.json(base);
+        } catch (err: any) {
+            console.log(err);
+            res.sendStatus(500);
+        }
     } else {
         res.sendStatus(404);
     }
 });
+
+/**
+ * Patch task by id, if cant find - send status 404. 
+ * If cause another error, send status 500
+ */
 app.patch('/tasks/:id', (req: Request, res: Response) => {
     let base = require('./db.json');
+    let updatedTask;
     try {
         const id = +req.params.id;
-        const updatedTask = patchTask(base, id, req.body);
-        save(base)
+        updatedTask = patchTask(base, id, req.body);
+        save(base);
         res.setHeader('Cache-Control', 'no-store, max-age=0');
         res.json(updatedTask);
-    } catch (err) {
-        res.send(err);
-    }
-});
-app.post('/tasks', urlencodedParser, (req: Request, res: Response) => {
-    let base = require('./db.json');
-    try {
-        console.log(req);
-        const createdTask = createTask(base, req.body);
-        save(base)
-        res.setHeader('Cache-Control', 'no-store, max-age=0');
-        res.json(createdTask);
-    } catch (err) {
-        res.send(err);
+    } catch (err: any) {
+        if (err.message === 'Not found') {
+            res.sendStatus(404);
+        } else {
+            console.log(err);
+            res.sendStatus(500);
+        }
     }
 });
 
+/**
+ * Create new task, if client send invalid structure - send status 400.
+ * If cause another error, send status 500
+ */
+app.post('/tasks', urlencodedParser, (req: Request, res: Response) => {
+    let base = require('./db.json');
+    let createdTask;
+    try {
+        console.log(req);
+        createdTask = createTask(base, req.body);
+        save(base);
+        res.setHeader('Cache-Control', 'no-store, max-age=0');
+        res.json(createdTask);
+    } catch (err: any) {
+        if (err.message === 'Invalid structure') {
+            res.sendStatus(400);
+        } else {
+            console.log(err);
+            res.sendStatus(500);
+        }
+    }
+});
+
+/**
+ * Dev function. Delete task by index in array, if cant find - send status 404. 
+ * If cause error, send status 500. Needed when an invalid date without ID is added to the array.
+ */
 app.delete('/tasks/devDelete/:id', (req: Request, res: Response) => {
     let base = require('./db.json');
     const id = +req.params.id;
     if (id < base.length) {
         base.splice(id, 1);
-        save(base)
-        res.setHeader('Cache-Control', 'no-store, max-age=0');
-        res.json(base);
+        try {
+            save(base);
+            res.setHeader('Cache-Control', 'no-store, max-age=0');
+            res.json(base);
+        } catch (err: any) {
+            console.log(err);
+            res.sendStatus(500);
+        }
     } else {
         res.sendStatus(404);
     }
 });
 
+/**
+ * Restore data base from backup. Set data to default. Send status 200 when success.
+ */
 app.get('/restoreDB', (req: Request, res: Response) => {
     try {
         const baseBackup = require('./db_backup.json');
@@ -106,6 +163,13 @@ app.get('/restoreDB', (req: Request, res: Response) => {
     }
 });
 
+/**
+ * Function that adds the required fields to the task and adds it to the database. 
+ * The function checks that all required fields are passed, otherwise it throws an error 'Invalid structure'
+ * @param {TaskInterface[]} base - Data base
+ * @param {TaskInterface} newTask - Incomplete task data received from the client
+ * @returns 
+ */
 function createTask(base: TaskInterface[], newTask: TaskInterface) {
     if (
         newTask.title == undefined ||
@@ -114,7 +178,7 @@ function createTask(base: TaskInterface[], newTask: TaskInterface) {
         newTask.category == undefined ||
         newTask.priority == undefined
     ) {
-        throw new Error('invalid structure');
+        throw new Error('Invalid structure');
     }
     newTask.id = ++tmpId;
     newTask.date = Date.now();
@@ -122,45 +186,48 @@ function createTask(base: TaskInterface[], newTask: TaskInterface) {
     return newTask;
 }
 
-function patchTask(base: TaskInterface[], id: number, taskData: Partial<TaskInterface>) {
+/**
+ * A function that makes changes to an existing task
+ * If taskData is undefined, throws error 'TaskData undefinded'.
+ * If there is no task with the specified id in the database, throws error 'Not found'.
+ * @param {TaskInterface[]} base - Data base 
+ * @param {number} id - Id of task that need to be changed
+ * @param {Partial<TaskInterface>} taskData - Contains fields that need to be changed
+ * @returns 
+ */
+function patchTask(
+    base: TaskInterface[],
+    id: number,
+    taskData: Partial<TaskInterface>
+) {
+    if (taskData == undefined) {
+        throw new Error('TaskData undefinded');
+    }
     const idInBase = base.findIndex((task: TaskInterface) => task.id === id);
     if (idInBase !== -1) {
         //нашли в базе такой элемент
-        if(taskData.title) base[idInBase].title = taskData.title;
-        if(taskData.description) base[idInBase].description = taskData.description;
-        if(taskData.status) base[idInBase].status = taskData.status;
-        if(taskData.category) base[idInBase].category = taskData.category;
-        if(taskData.priority) base[idInBase].priority = taskData.priority;
+        if (taskData.title) base[idInBase].title = taskData.title;
+        if (taskData.description)
+            base[idInBase].description = taskData.description;
+        if (taskData.status) base[idInBase].status = taskData.status;
+        if (taskData.category) base[idInBase].category = taskData.category;
+        if (taskData.priority) base[idInBase].priority = taskData.priority;
         return base[idInBase];
     } else {
-        throw new Error('not found');
+        throw new Error('Not found');
     }
 }
 
+/**
+ * Save data base to file. 
+ * (In vercel causes error 'EROFS', that can be ignored. On server this error dont causes.)
+ * @param {TaskInterface[]} base - Data base  
+ */
 function save(base: TaskInterface[]) {
     fs.writeFileSync(filePathInScriptDir, JSON.stringify(base), (err: any) => {
-        if (err) throw err;
+        if (err.code != 'EROFS') throw err;
     });
-    // fs.writeFileSync('db.json', JSON.stringify(base), (err: any) => {
-    //     if (err) throw err;
-    // });
-    // fs.writeFileSync('./db.json', JSON.stringify(base), (err: any) => {
-    //     if (err) throw err;
-    // });
-    // fs.writeFileSync('./dist/db.json', JSON.stringify(base), (err: any) => {
-    //     if (err) throw err;
-    // });
-    // fs.writeFileSync('dist/db.json', JSON.stringify(base), (err: any) => {
-    //     if (err) throw err;
-    // });
-    // fs.writeFileSync('./api/db.json', JSON.stringify(base), (err: any) => {
-    //     if (err) throw err;
-    // });
-    // fs.writeFileSync('/api/db.json', JSON.stringify(base), (err: any) => {
-    //     if (err) throw err;
-    // });
 }
-
 
 app.listen(3000, () => console.log('Server ready on port 3000.'));
 module.exports = app;
